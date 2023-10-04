c9dff6881ef1fd1e55d2c486c5388c144c43aa2d
Fei <ozzyfly@yahoo.com.tw>
2023-10-03 17:23:04 -0700

    update StackNavigator.js

--- src/navigation/StackNavigator.js
+++ src/navigation/StackNavigator.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import AllExpenses from '../screens/AllExpenses';
+import OverbudgetExpenses from '../screens/OverbudgetExpenses';
 import AddExpense from '../screens/AddExpense';
+import EditExpense from '../screens/EditExpense';
 
 const Stack = createStackNavigator();
 
@@ -9,8 +11,9 @@ function StackNavigator() {
   return (
     <Stack.Navigator initialRouteName="AllExpenses">
       <Stack.Screen name="AllExpenses" component={AllExpenses} />
+      <Stack.Screen name="OverbudgetExpenses" component={OverbudgetExpenses} />
       <Stack.Screen name="AddExpense" component={AddExpense} />
-      {/* Add other screens as needed */}
+      <Stack.Screen name="EditExpense" component={EditExpense} />
     </Stack.Navigator>
   );
 }
