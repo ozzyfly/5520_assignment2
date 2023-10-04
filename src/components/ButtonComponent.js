c3dc4992c97e310b6697bcdc60c8292addb17016
Fei <ozzyfly@yahoo.com.tw>
2023-10-03 17:06:57 -0700

    edit ButtonComponent.js

---
+++ src/components/ButtonComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { commonStyles } from '../styles/commonStyles';
+
+const ButtonComponent = ({ title, onPress }) => {
+  return (
+    <TouchableOpacity style={commonStyles.button} onPress={onPress}>
+      <Text style={commonStyles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default ButtonComponent;
