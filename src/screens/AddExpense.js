5a110f86ad7693820bc4dad6da528f9667130ed6
Fei <ozzyfly@yahoo.com.tw>
2023-10-03 22:18:03 -0700

    Incorporate the DropdownPicker and add validation for the form fields.

--- src/screens/AddExpense.js
+++ src/screens/AddExpense.js
@@ -1,15 +1,27 @@
 import React, { useState } from 'react';
-import { View, TextInput, Text } from 'react-native';
+import { View, TextInput, Text, Alert } from 'react-native';
 import ButtonComponent from '../components/ButtonComponent';
+import DropdownPicker from '../components/DropdownPicker';
 import { commonStyles } from '../styles/commonStyles';
 
 const AddExpense = () => {
   const [name, setName] = useState('');
   const [price, setPrice] = useState('');
+  const [quantity, setQuantity] = useState('1');
 
   const handleSave = () => {
-    // Add saving logic here
-    console.log(name, price);
+    if (!name || !price) {
+      Alert.alert('Error', 'Please fill out all fields.');
+      return;
+    }
+
+    if (isNaN(price) || price <= 0) {
+      Alert.alert('Error', 'Enter a valid price.');
+      return;
+    }
+
+    console.log(name, price, quantity);
+    // Logic to save the data would be placed here
   };
 
   return (
@@ -26,6 +38,17 @@ const AddExpense = () => {
         onChangeText={setPrice}
         placeholder="Enter price"
         keyboardType="numeric"
+      />
+      <Text>Quantity:</Text>
+      <DropdownPicker 
+        selectedValue={quantity}
+        onValueChange={(value) => setQuantity(value)}
+        items={[
+          { label: '1', value: '1' },
+          { label: '2', value: '2' },
+          { label: '3', value: '3' },
+          // ... you can expand this list
+        ]}
       />
       <ButtonComponent title="Save" onPress={handleSave} />
     </View>
